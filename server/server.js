9983b3b3997c0d3835fe60d80948ed128509d253
myadavaramshravan-ship-it <myadavaramshravan@gmail.com>
2026-07-10 00:18:00 +0530

    Connect MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 const app = express();
 
-// Middleware
+// Connect Database
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
